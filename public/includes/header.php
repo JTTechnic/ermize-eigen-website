6e5de8e4a39c4d6bc9fdfd99d466b6c617ae4ae8
DirtKingy <92637772+DirtKingy@users.noreply.github.com>
2024-09-05 12:13:52 +0200

    updated the ehader so it uses the session data to display the correct nav elements

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,7 +1,17 @@
+<?php
+session_start();
+?>
+
 <header>
     <nav>
         <a href="index.php">Home</a>
-        <a href="login.php">Login</a>
-        <a href="register.php">Register</a>
+        
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="profile.php">Profile</a>
+            <a href="logout.php">Logout</a>
+        <?php else: ?>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+        <?php endif; ?>
     </nav>
 </header>

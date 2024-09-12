62b3dcb6091e7474696d9c5aeb39983083af833c
DirtKingy <92637772+DirtKingy@users.noreply.github.com>
2024-09-12 10:06:52 +0200

    fixed that the program switches css sheets for better use

--- public/includes/head.php
+++ public/includes/head.php
@@ -2,5 +2,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ERMize</title>
-    <link rel="stylesheet" href="css/style.css">
+
+    <?php
+    $page = basename($_SERVER['PHP_SELF'], ".php");
+
+    if (in_array($page, ['login', 'register', 'profile'])) {
+        echo '<link rel="stylesheet" href="css/login.css">';
+    } else {
+        echo '<link rel="stylesheet" href="css/style.css">';
+    }
+    ?>
 </head>
--- public/index.php
+++ public/index.php
@@ -4,7 +4,14 @@
 <body>
     <?php require_once('includes/header.php'); ?>
     <main>
-
+        <h2>Info board:</h2>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+        </ul>
     </main>
     <?php require_once('includes/footer.php'); ?>
 </body>
